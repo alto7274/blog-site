5da78721327cf49d359ccfeb6383a5185d702bdb
Altamash Tamboli <altamashtamboli48@gmail.com>
2023-05-14 11:35:04 +0530

    $1

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+!#/bin/bash
+
+echo "deploying script started fuck you motherfucker madarchod wooshang gang"
+
+hugo 
+cd public
+git add . ; git commit -m '$1' ; git push origin main
+cd ..
+git add . ; git commit -m '$1' ; git push origin main
+
+
+
+
+echo 'madarchod deploy ho gaya hai live hoga 2 min mein lavde'
